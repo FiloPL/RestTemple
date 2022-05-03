d5511779fc17ea5655d959a4d934d67f846bf715
FiloPL <tzegarlicki@gmail.com>
2022-05-03 21:52:17 +0200

    add pojo

---
+++ src/main/java/com/filo/resttemple/domain/Billing.java
@@ -0,0 +1,32 @@
+package com.filo.resttemple.domain;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by T. Filo Zegarlicki on 03.05.2022
+ **/
+public class Billing implements Serializable {
+
+    private Card card;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 6577338081290507077L;
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
